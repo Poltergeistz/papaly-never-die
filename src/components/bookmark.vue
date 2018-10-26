fc57b83757358384e86c1077f0dfa59beaeae145
david <david.journo.31@gmail.com>
2018-10-26 15:42:26 +0200

    Board component with links are categories

--- src/components/bookmark.vue
+++ src/components/bookmark.vue
@@ -1,31 +1,128 @@
 <template>
 <div>
-  <div class="container">
-    <div class="form">
-    <div class="form-group">
-      <div class="form-group">
-        <label>Bookmark link</label>
-        <input class="form-control" type="text" v-model="link">
-      </div>
-    </div>
-    <button class="btn btn-primary" @click="addBookmark">Submit</button>
-  </div>
-  <div class="col-sm-12">
-    <div class="col-sm-4" v-for="(bookmark, index) in bookmarks" v-bind:key="index">
-      <div class="alert alert-primary" role="alert">
-        <button class="close" @click="removeBookmark(index)">&times;</button>
-        <a v-bind:href="bookmark" class="card-title">{{bookmark}}</a>
-        Modifier lien : <input type="text" v-model="editLink">
-      </div>
-    </div>
-  </div>
-  </div>
+  <v-layout justify-center>
+    <!-- Board -->
+    <v-flex xs12 sm10 md10>
+      <v-card>
+    <!-- Categories -->
+    <v-flex xs12 sm6 md4>
+    <v-card>
+      <v-card-text 
+        class="headline blue lighten-5"
+        primary-title>
+        Category
+        <!-- Modal add categories -->
+    <v-dialog
+        v-model="dialog"
+        width="500"
+      >
+      <v-icon slot="activator">add</v-icon>
+        <v-card>
+          <v-card-title
+            class="headline blue lighten-5"
+            primary-title
+          >
+            New Category
+          </v-card-title>
+
+          <v-card-text>
+          <v-text-field label="Category Name"></v-text-field>
+          </v-card-text>
+  
+          <v-divider></v-divider>
+  
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn flat @click="dialog = false">Cancel</v-btn>
+            <v-btn
+              color="success"
+              flat
+              @click="dialog = false"
+            >
+              Add
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+      <!-- Modal end -->
+      </v-card-text>
+      <v-container
+      fluid
+      grid-list-md
+      ><v-text-field label="bookmark link"  v-model="link"></v-text-field>
+      <v-btn @click="addBookmark">Link<v-icon right>fas fa-link</v-icon></v-btn>
+      <!-- Links -->
+      <v-card wrap sx6 md6 md4 v-for="(bookmark, index) in bookmarks" v-bind:key="index">
+        <v-card color="blue lighten-4" sx6 md6 md4>
+          <!-- Modal edit link -->
+          <v-dialog
+        v-model="dialog2"
+        width="500"
+      >
+        <v-btn
+          slot="activator"
+          color="red lighten-2"
+          dark
+        >
+          <v-icon>fas fa-pencil-alt</v-icon>
+        </v-btn>
+  
+        <v-card>
+          <v-card-title
+            class="headline blue lighten-5"
+            primary-title
+          >
+            Modify link
+          </v-card-title>
+
+          <v-card-text>
+          <v-text-field label="link name">{{bookmark}}</v-text-field>
+          <v-text-field label="link url"></v-text-field>
+          <v-textarea
+            name="input-7-4"
+            label="Short resume"
+            value=""
+            no-resize
+          ></v-textarea>
+          <p class="font-italic caption">Max char 100</p>
+          </v-card-text>
+  
+          <v-divider></v-divider>
+  
+          <v-card-actions>
+              <v-btn color="deep-orange darken-3" @click="removeBookmark(index)" flat>Delete this link</v-btn>
+            <v-spacer></v-spacer>
+            <v-btn flat @click="dialog2 = false">Cancel</v-btn>
+            <v-btn
+              color="success"
+              flat
+              v-model="editLink"
+              @click="dialog2 = false"
+            >
+              Accept
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+      <!-- Modal end -->
+          <a v-bind:href="bookmark" class="card-title">{{bookmark}}</a>
+        </v-card>
+      </v-card>
+      </v-container>
+    </v-card>
+    </v-flex>
+    </v-card>
+    </v-flex>
+    </v-layout>
+    
   </div>
 </template>
 <script>
     export default {
         data(){
           return {
+             dialog:false,
+             dialog2:false,
              bookmarks: []
             }
              },
