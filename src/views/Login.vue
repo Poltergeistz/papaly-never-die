0bd18fed27ded4d5a8c18c4c8a8c009907404f74
david <david.journo.31@gmail.com>
2018-10-27 18:11:21 +0200

    Implementing firebase login system

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,7 +3,7 @@
         <v-responsive>
             <h4>Login</h4>
             <v-form>
-            <v-text-field label="email adress"></v-text-field>
+            <v-text-field label="email adress" v-model="email"></v-text-field>
             <v-text-field
               :append-icon="show3 ? 'visibility_off' : 'visibility'"
               :rules="[rules.required, rules.min]"
@@ -14,24 +14,44 @@
               value=""
               class="input-group--focused"
               @click:append="show3 = !show3"
+              v-model="password"
             ></v-text-field>
+            <v-btn @click="login">
+                Login
+            </v-btn>
             </v-form>
         </v-responsive>
     </v-container>
 </template>
 <script>
+import firebase from 'firebase'
 
-    export default{
-        data () {
-            return {
-                show3: false,
-                password: 'Password',
-                rules: {
-                required: value => !!value || 'Required.',
-                min: v => v.length >= 8 || 'Min 8 characters',
-                emailMatch: () => ('The email and password you entered don\'t match')
+export default {
+    data(){
+        return{
+            email: '',
+            password: '',
+            show3: false,
+            rules: {
+            required: value => !!value || 'Required.',
+            min: v => v.length >= 8 || 'Min 8 characters',
+            emailMatch: () => ('The email and password you entered don\'t match')
+            }
+        }
+    },
+    methods: {
+        login: (e) => {
+            firebase.auth.signInWithEmailAndPassword(this.email, this.password)
+            .then(user => {
+                alert(`Logged as ${user.email}`)
+                this.$router.push('/')
+            }, error => {
+                alert(error.message)
+            })
+            // console.log(login)
+            e.preventDefault()
+        }
     }
-  }
-}
+    
 }
 </script>
--- src/views/Signin.vue
+++ src/views/Signin.vue
@@ -3,7 +3,7 @@
         <v-responsive>
             <h4>Sign In</h4>
             <v-form>
-            <v-text-field label="email adress"></v-text-field>
+            <v-text-field label="email adress" v-model="email"></v-text-field>
             <v-text-field
               :append-icon="show3 ? 'visibility_off' : 'visibility'"
               :rules="[rules.required, rules.min]"
@@ -14,9 +14,10 @@
               value=""
               class="input-group--focused"
               @click:append="show3 = !show3"
+              v-model="password"
             ></v-text-field>
   
-            <v-text-field
+            <!-- <v-text-field
               :append-icon="show4 ? 'visibility_off' : 'visibility'"
               :rules="[rules.required, rules.emailMatch]"
               :type="show4 ? 'text' : 'password'"
@@ -26,25 +27,44 @@
               value=""
               error
               @click:append="show4 = !show4"
-            ></v-text-field>
+            ></v-text-field> -->
+            <v-btn @click="register">
+                Sign in
+            </v-btn>
             </v-form>
         </v-responsive>
     </v-container>
 </template>
 <script>
+import firebase from 'firebase'
 
-    export default{
-        data () {
-            return {
-                show3: false,
-                show4: false,
-                password: 'Password',
-                rules: {
-                required: value => !!value || 'Required.',
-                min: v => v.length >= 8 || 'Min 8 characters',
-                emailMatch: () => ('The email and password you entered don\'t match')
+export default {
+    data(){
+        return{
+            email: '',
+            password: '',
+            show3: false,
+            show4: false,
+            rules: {
+            required: value => !!value || 'Required.',
+            min: v => v.length >= 8 || 'Min 8 characters',
+            emailMatch: () => ('The email and password you entered don\'t match')
+            }
+        }
+    },
+    methods: {
+        register: (e) => {
+            firebase.auth().createUserWithEmailAndPassword(this.email, this.password)
+            .then(user => {
+                alert(`Account created for ${user.email}`)
+                this.$router.push('/')
+            }, 
+            error => {
+                alert(error.message)
+            }) 
+            // console.log(register)
+            e.preventDefault()
+        }
     }
-  }
-}
 }
 </script>
