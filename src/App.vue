cfe7d13da113be2ffb0b85308f6f85a000610255
david <david.journo.31@gmail.com>
2018-10-27 18:09:35 +0200

    Show routes if logged or not

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,49 @@
 <template>
   <v-app>
-    <v-toolbar color="blue lighten-3" app>
+    <v-toolbar color="blue lighten-3" app :key="$route.fullPath">
       <v-toolbar-title class="headline text-uppercase">
         <span>Papaly</span>
         <span class="font-weight-light">NEVER DIE</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <router-link to="/" class="link"><v-btn flat>Board</v-btn></router-link>
-      <router-link to="/login" class="link"><v-btn flat>Login</v-btn></router-link>
-      <router-link to="/signin" class="link"><v-btn flat>Sign In</v-btn></router-link>
+      <v-toolbar-items>
+      <ul>
+      <span v-if="isLoggedIn">{{currentUser.email}}</span>
+      <li v-if="!isLoggedIn"><router-link to="/">Board</router-link></li>
+      <li v-if="isLoggedIn"><router-link to="/editboard">Edit Board</router-link></li>
+      <li v-if="!isLoggedIn"><router-link to="/login">Login</router-link></li>
+      <li v-if="!isLoggedIn"><router-link to="/signin">Sign In</router-link></li>
+      <v-btn v-if="isLoggedIn" flat @click="logout()">Logout</v-btn>
+      </ul>
+      </v-toolbar-items>
     </v-toolbar>
     <v-content>
       <router-view/>
     </v-content>
   </v-app>
 </template>
+<script>
+import firebase from 'firebase'
 
+export default {
+  data(){
+    return {
+      isLoggedIn: false,
+      currentUser: false
+    }
+  },
+  created(){
+    if(firebase.auth().currentUser){
+      this.isLoggedIn = true
+      this.currentUser = firebase.auth().currentUser.email
+    }
+  },
+  methods: {
+    logout: () => {
+      firebase.auth().signOut().then( () => {
+        this.$router.push('/login')
+      })
+    }
+  }
+}
+</script>
--- src/router.js
+++ src/router.js
@@ -1,24 +1,78 @@
 import Vue from "vue";
 import Router from "vue-router";
+import firebase from 'firebase';
 
 Vue.use(Router);
 
-export default new Router({
+let router = new Router({
   routes: [
     {
       path: "/",
       name: "board",
-      component: () => import("./views/Board.vue")
+      component: () => import("./views/Board.vue"),
+      meta: {
+        requiresGuest: true
+      }
+    },
+    {
+      path: "/editboard",
+      name: "editboard",
+      component: () => import("./views/Edit_board.vue"),
+      meta: {
+        requiresAuth: true
+      }
     },
     {
       path: "/login",
       name: "login",
-      component: () => import("./views/Login.vue")
+      component: () => import("./views/Login.vue"),
+      meta: {
+        requiresGuest: true
+      }
     },
     {
       path: "/signin",
       name: "signin",
-      component: () => import("./views/Signin.vue")
+      component: () => import("./views/Signin.vue"),
+      meta: {
+        requiresGuest: true
+      }
     }
   ]
 });
+
+//
+
+router.beforeEach((to, from, next) => {
+  // RequiredAuth
+  if(to.matched.some(record => {record.meta.requiresAuth})){
+    // If not logged in
+    if(!firebase.auth().currentUser){
+      next({ path: '/login',
+      query: {
+        redirect: to.fullPath
+      }
+    })
+    } else {
+      // Proceed to route
+      next()
+    }
+  } else if(to.matched.some(record => {record.meta.requiresGuest})) {
+    // If logged in
+    if(firebase.auth().currentUser){
+      next({ path: '/',
+      query: {
+        redirect: to.fullPath
+      }
+    })
+    } else {
+    // Proceed to route
+    next()
+    }
+  } else {
+    // Proceed to route
+    next()
+  }
+})
+
+export default router;
