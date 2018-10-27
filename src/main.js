8f7ce246833b27eddc352d0084db2a862aa0c6bc
DavidJ <38471754+Poltergeistz@users.noreply.github.com>
2018-10-27 21:02:13 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,18 +5,7 @@ import App from './App.vue'
 import router from './router'
 import firebase from 'firebase'
 
-
 Vue.config.productionTip = false
-
-var configuration = {
-  apiKey: "AIzaSyB8QskB2aAvzF90q68EkPRCtT8ay30NMtY",
-  authDomain: "papaly-never-die.firebaseapp.com",
-  databaseURL: "https://papaly-never-die.firebaseio.com",
-  projectId: "papaly-never-die",
-  storageBucket: "papaly-never-die.appspot.com",
-  messagingSenderId: "496040267197"
-};
-firebase.initializeApp(configuration);
 
 let app;
 firebase.auth().onAuthStateChanged(user => {
