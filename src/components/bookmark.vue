d6b98c5c2ccfcf48b4f9851d59e34886fa419729
david <david.journo.31@gmail.com>
2018-10-28 17:44:43 +0100

    Fetching data from db + some design

--- src/components/bookmark.vue
+++ src/components/bookmark.vue
@@ -3,114 +3,75 @@
   <v-layout justify-center>
     <!-- Board -->
     <v-flex xs12 sm10 md10>
+      <AddCategoryModal/>
       <v-card>
     <!-- Categories -->
     <v-flex xs12 sm6 md4>
     <v-card>
-      <v-card-text 
-        class="headline blue lighten-5"
+      <v-toolbar
+        class="headline blue lighten-5 elevation-0"
         primary-title>
-        Category
-        <!-- Modal add categories -->
-    <v-dialog
-        v-model="dialog"
-        width="500"
-      >
-      <v-icon slot="activator">add</v-icon>
-        <v-card>
-          <v-card-title
-            class="headline blue lighten-5"
-            primary-title
-          >
-            New Category
-          </v-card-title>
-
-          <v-card-text>
-          <v-text-field label="Category Name"></v-text-field>
-          </v-card-text>
-  
-          <v-divider></v-divider>
-  
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn flat @click="dialog = false">Cancel</v-btn>
-            <v-btn
-              color="success"
-              flat
-              @click="dialog = false"
-            >
-              Add
+        {{category_name}}
+        <v-spacer></v-spacer>
+        <!-- menu -->
+        <v-menu :nudge-width="100"> 
+            <v-btn slot="activator" icon>
+        <v-icon>more_vert</v-icon>
             </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
-      <!-- Modal end -->
-      </v-card-text>
+  
+          <v-list>
+            <v-list-tile>
+              <AddLinkModal/>
+              <EditCategoryModal/>
+            </v-list-tile>
+          </v-list>
+        </v-menu>
+        <!-- menu end -->
+      </v-toolbar>
       <v-container
       fluid
       grid-list-md
-      ><v-text-field label="bookmark link"  v-model="link"></v-text-field>
-      <v-btn @click="addBookmark">Link<v-icon right>fas fa-link</v-icon></v-btn>
-      <!-- Links -->
-      <v-card wrap sx6 md6 md4 v-for="(bookmark, index) in bookmarks" v-bind:key="index">
-        <v-card color="blue lighten-4" sx6 md6 md4>
-          <!-- Modal edit link -->
-          <v-dialog
-        v-model="dialog2"
-        width="500"
       >
-        <v-btn
-          slot="activator"
-          color="red lighten-2"
-          dark
-        >
-          <v-icon>fas fa-pencil-alt</v-icon>
-        </v-btn>
-  
-        <v-card>
-          <v-card-title
-            class="headline blue lighten-5"
-            primary-title
-          >
-            Modify link
-          </v-card-title>
-
-          <v-card-text>
-          <v-text-field label="link name">{{bookmark}}</v-text-field>
-          <v-text-field label="link url"></v-text-field>
-          <v-textarea
-            name="input-7-4"
-            label="Short resume"
-            value=""
-            no-resize
-          ></v-textarea>
-          <p class="font-italic caption">Max char 100</p>
-          </v-card-text>
-  
-          <v-divider></v-divider>
-  
-          <v-card-actions>
-              <v-btn color="deep-orange darken-3" @click="removeBookmark(index)" flat>Delete this link</v-btn>
-            <v-spacer></v-spacer>
-            <v-btn flat @click="dialog2 = false">Cancel</v-btn>
-            <v-btn
-              color="success"
-              flat
-              v-model="editLink"
-              @click="dialog2 = false"
-            >
-              Accept
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
-      <!-- Modal end -->
-          <a v-bind:href="bookmark" class="card-title">{{bookmark}}</a>
+      <!-- Links -->
+      <v-card wrap sx6 md6 md4>
+        <!-- v-for="(bookmark, index) in bookmarks" v-bind:key="index" -->
+        <v-card color="blue lighten-4" sx6 md6 md4>
+          <EditLinkModal/>
+          <a class="card-title">{{link_url}}</a>
         </v-card>
       </v-card>
       </v-container>
+    <!-- New categories -->  
+    <v-card wrap sx6 md6 md4>
+      <!-- v-for="(category, index) in categories" v-bind:key="index" -->
+      <v-toolbar
+        class="headline blue lighten-5 elevation-0"
+        primary-title>{{category_name}}
+        
+        <v-spacer></v-spacer>
+        <!-- menu -->
+        <v-menu :nudge-width="100"> 
+            <v-btn slot="activator" icon>
+        <v-icon>more_vert</v-icon>
+            </v-btn>
+  
+          <v-list>
+            <v-list-tile>
+              <AddLinkModal/>
+              <EditCategoryModal/>
+            </v-list-tile>
+          </v-list>
+        </v-menu>
+        <!-- menu end -->
+      </v-toolbar>
+        <v-container
+      fluid
+      grid-list-md></v-container>
+    </v-card>
+    <!-- Category card end -->  
     </v-card>
     </v-flex>
+    <!-- Board end -->
     </v-card>
     </v-flex>
     </v-layout>
@@ -118,39 +79,56 @@
   </div>
 </template>
 <script>
+//import db from '../firebaseinit.js'
+import Modal from '../components/Modal'
+import AddCategoryModal from '../components/AddCategoryModal'
+import EditCategoryModal from '../components/EditCategoryModal'
+import AddLinkModal from '../components/AddLinkModal'
+import EditLinkModal from '../components/EditLinkModal'
+import db from '../firebaseinit.js'
     export default {
+      components: {
+        Modal,
+        AddCategoryModal,
+        AddLinkModal,
+        EditCategoryModal,
+        EditLinkModal
+        },
         data(){
           return {
-             dialog:false,
-             dialog2:false,
-             bookmarks: []
+             category_name: null,
+             link_name: null,
+             link_url: null,
+             link_desc: null
             }
-             },
+            },
+        watch: {
+          
+        },
         methods: {
-        addBookmark() {
-          this.bookmarks.push(this.link)
+          fetchCategories(){
+          // Fetch all categories
+          db.collection('category').get().then(querySnapshot => {
+            querySnapshot.forEach(doc => {
+              this.category_name = doc.data().category_name
+            })
+          })
           },
-        removeBookmark(index) {
-          this.bookmarks.splice(index, 1)
-          }
-  },
-  computed: {
-    editLink: {
-      get: function(){
-        return this.link
-      },
-      set: function(value){
-        this.link = value
-        console.log(value)
-      }
-    }
-  }
+          fetchLinks(){
+          // Fetch all links
+          db.collection('link').get().then(querySnapshot => {
+            querySnapshot.forEach(doc => {
+              this.link_url = doc.data().link_url
+            })
+          })
+        }
+        }
 }
 </script>
 <style>
 
 a{
-  color:#42b983;
+  color:#424eb9;
 }
 </style>
 
