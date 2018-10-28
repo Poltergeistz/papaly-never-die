9a13d405b96c4c6324121388521ef9fe6e82615b
david <david.journo.31@gmail.com>
2018-10-28 17:43:51 +0100

    Building a menu

--- src/App.vue
+++ src/App.vue
@@ -7,14 +7,30 @@
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items>
-      <ul>
-      <span v-if="isLoggedIn">{{currentUser.email}}</span>
-      <li v-if="!isLoggedIn"><router-link to="/">Board</router-link></li>
-      <li v-if="isLoggedIn"><router-link to="/editboard">Edit Board</router-link></li>
-      <li v-if="!isLoggedIn"><router-link to="/login">Login</router-link></li>
-      <li v-if="!isLoggedIn"><router-link to="/signin">Sign In</router-link></li>
+        <span v-if="isLoggedIn">{{currentUser.email}}</span>
+        <!-- menu -->
+        <v-menu :nudge-width="100"> 
+            <!-- <v-btn slot="activator" icon>
+        <v-icon>more_vert</v-icon>
+            </v-btn> -->
+            <v-toolbar-side-icon slot="activator"></v-toolbar-side-icon>
+          <v-list>
+            <v-list-tile v-if="isLoggedIn">
+              <router-link to="/editboard">Edit Board</router-link>
+            </v-list-tile>
+            <v-list-tile v-if="!isLoggedIn">
+                <router-link to="/">Board</router-link>
+            </v-list-tile>
+            <v-list-tile v-if="!isLoggedIn">
+              <router-link to="/login">Login</router-link>             
+            </v-list-tile>
+            <v-list-tile v-if="!isLoggedIn">
+              <router-link to="/signin">Register</router-link>
+            </v-list-tile>
+          </v-list>
+        </v-menu>
+        <!-- menu end -->
       <v-btn v-if="isLoggedIn" flat @click="logout()">Logout</v-btn>
-      </ul>
       </v-toolbar-items>
     </v-toolbar>
     <v-content>
